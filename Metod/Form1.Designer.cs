c863ce1ad67bf352daedeb247a054c5c4a80ad15
M4zOk <maz-ign@yandex.ru>
2022-11-25 20:47:03 +0700

    Добавьте файлы проекта.

---
+++ Metod/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Metod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.raschet_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Otvlbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LeftGran = new System.Windows.Forms.TextBox();
+            this.RigntGran = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // raschet_btn
+            // 
+            this.raschet_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.raschet_btn.Location = new System.Drawing.Point(450, 483);
+            this.raschet_btn.Name = "raschet_btn";
+            this.raschet_btn.Size = new System.Drawing.Size(126, 35);
+            this.raschet_btn.TabIndex = 0;
+            this.raschet_btn.Text = "Расчитать";
+            this.raschet_btn.UseVisualStyleBackColor = true;
+            this.raschet_btn.Click += new System.EventHandler(this.raschet_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(14, 483);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите число разбиений отрезка";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(344, 488);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(612, 370);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(14, 534);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(299, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Расчетное значение интеграла:";
+            // 
+            // Otvlbl
+            // 
+            this.Otvlbl.AutoSize = true;
+            this.Otvlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Otvlbl.Location = new System.Drawing.Point(339, 534);
+            this.Otvlbl.Name = "Otvlbl";
+            this.Otvlbl.Size = new System.Drawing.Size(0, 25);
+            this.Otvlbl.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(14, 429);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Введите границы от:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(319, 429);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "до";
+            // 
+            // LeftGran
+            // 
+            this.LeftGran.Location = new System.Drawing.Point(213, 433);
+            this.LeftGran.Name = "LeftGran";
+            this.LeftGran.Size = new System.Drawing.Size(100, 20);
+            this.LeftGran.TabIndex = 13;
+            this.LeftGran.Text = "0";
+            // 
+            // RigntGran
+            // 
+            this.RigntGran.Location = new System.Drawing.Point(358, 434);
+            this.RigntGran.Name = "RigntGran";
+            this.RigntGran.Size = new System.Drawing.Size(100, 20);
+            this.RigntGran.TabIndex = 14;
+            this.RigntGran.Text = "3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 579);
+            this.Controls.Add(this.RigntGran);
+            this.Controls.Add(this.LeftGran);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Otvlbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.raschet_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button raschet_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Otvlbl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox LeftGran;
+        private System.Windows.Forms.TextBox RigntGran;
+    }
+}
+
---
+++ Metod/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        static double f(double x)
+        {
+            return (x / 4.5) * Math.Exp((Math.Pow(x, 2) * -1) / 8.2);
+        }
+
+
+        static double Priamougol(double a, double b, int n)
+        {
+            double result, h;
+
+            h = (b - a) / n;
+            result = 0.0;
+            for (int i = 0; i <= n; i++)
+            {
+                result += f(a + h * (i - 0.5));
+            }
+            result *= h;
+            return result;
+        }
+
+
+
+        private void raschet_btn_Click(object sender, EventArgs e)
+        {
+
+            double a2 , b2 , h2 = 1, y, x2;
+            this.chart1.Series[0].Points.Clear();
+            a2 = Convert.ToInt32(LeftGran.Text);
+            b2 = Convert.ToInt32(RigntGran.Text);
+            x2 = a2;
+            while (a2 <= b2)
+            {
+                y = (x2 / 4.5) * Math.Exp((Math.Pow(x2, 2)* -1) / 8.2);
+                this.chart1.Series[0].Points.AddXY(x2, y);
+                x2 += h2;
+                a2 += 1;
+            }
+
+            double a = 0, b = 3;
+            int n;
+            n = Convert.ToInt32(textBox1.Text);
+            double z = Priamougol(a,b,n);
+            Otvlbl.Text = z.ToString();
+            
+        }
+
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
